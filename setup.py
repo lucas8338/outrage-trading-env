1f75be6d3ff7191760f9b65319d42f6e72031d67
DESKTOP-P4RI119\Lucas <lucas.ma8338@gmail.com>
2021-09-11 18:05:50 -0300

    1.0.0 dist

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['outrage_trading_env']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gym>=0,<1', 'numpy>=1,<2', 'pandas>=1,<2', 'sklearn>=0,<1']
+
+setup_kwargs = {
+    'name': 'outrage-trading-env',
+    'version': '1.0.0',
+    'description': 'A gym-env to do trading (forex/stock/crypto) using reinforcement-learning',
+    'long_description': None,
+    'author': 'Lucas Monteiro',
+    'author_email': 'lucas.ma8338@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3',
+}
+
+
+setup(**setup_kwargs)
